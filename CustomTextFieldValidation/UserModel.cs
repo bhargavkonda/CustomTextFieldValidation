9ec8f6ee512d8f0693e46d4fba5a01e02c8a6f53
bhkond <bhkond@gmail.com>
2024-06-03 23:27:05 +0530

    Add project files.

---
+++ CustomTextFieldValidation/UserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CustomTextFieldValidation
+{
+    public class UserModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ CustomTextFieldValidation/UserModelValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace CustomTextFieldValidation
+{
+    public class UserModelValidator : AbstractValidator<UserModel>
+    {
+        public UserModelValidator()
+        {
+            RuleFor(x => x.FirstName)
+                .NotEmpty().WithMessage("First Name is required.")
+                .MaximumLength(50).WithMessage("First Name cannot exceed 50 characters.");
+
+            RuleFor(x => x.LastName)
+                .NotEmpty().WithMessage("Last Name is required.")
+                .MaximumLength(50).WithMessage("Last Name cannot exceed 50 characters.");
+
+            RuleFor(x => x.Status)
+                .NotEmpty().WithMessage("Status is required.")
+                .MaximumLength(20).WithMessage("Status cannot exceed 20 characters.");
+        }
+    }
+}
